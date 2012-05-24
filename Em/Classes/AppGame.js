f9bc2d217712b9e4dc7cbb438d42cf6c1dd4d3fd
ShengxiangChen <shengxiang.chen@cocos2d-x.org>
2012-05-24 11:49:34 +0800

    1

--- Em/Classes/AppGame.js
+++ Em/Classes/AppGame.js
@@ -1,5 +1,30 @@
+var Ship = cc.Sprite.extend({
+    ctor:function(){
+        this.initWithFile(s_ship01);
+    }
+});
 var spaceGame = cc.Layer.extend({
+    _ship:null,
+    onEnter:function(){
+        //bg
+        var bg01 = cc.Sprite.spriteWithFile(s_bg01);
+        bg01.setAnchorPoint(cc.ccp(0, 0));
+        var voidNode = cc.ParallaxNode.node();
+        voidNode.addChild(bg01, -1, cc.ccp(0, 0), cc.PointZero());
 
+        var goUp = cc.MoveBy.actionWithDuration(4, cc.ccp(0, 100));
+        var goDown = goUp.reverse();
+        var go = cc.MoveBy.actionWithDuration(8, cc.ccp(200, 0));
+        var goBack = go.reverse();
+        var seq = cc.Sequence.actions(goUp, go, goDown, goBack, null);
+        voidNode.runAction(cc.RepeatForever.actionWithAction(seq));
+        this.addChild(voidNode,0);
+
+        //ship
+        this._ship = new Ship();
+        this._ship.setPosition(cc.ccp(100,100));
+
+    }
 });
 
 spaceGame.node = function () {
--- Em/Resource.js
+++ Em/Resource.js
@@ -1,5 +1,8 @@
-var s_plane = "Resources/plane.png";
+var s_bg01 = "Resources/bg01.jpg";
+var s_loading = "Resources/loading.jpg";
+var s_ship01 = "Resources/ship01.png";
 
 var g_ressources = [
-    //{type:"image", src:s_plane},
+    {type:"image", src:s_bg01},
+    {type:"image", src:s_ship01},
 ];
